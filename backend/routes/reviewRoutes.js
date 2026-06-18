7ef255db92523fae81ddb7ea8408c3103a388876
abhishek01rawat <39910936+abhishek01rawat@users.noreply.github.com>
2026-06-18 10:34:53 +0530

    Delete backend directory

--- backend/routes/reviewRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { deleteReview } from '../controllers/reviewController.js';
-import { protect } from '../middleware/auth.js';
-const router = express.Router();
-router.delete('/:id', protect, deleteReview);
-export default router;
