337f8835b1701dcbb5b3228707b310d84621e548
abhishek01rawat <39910936+abhishek01rawat@users.noreply.github.com>
2026-06-18 16:33:21 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({
-  bbaseUrl: import.meta.env.VITE_API_URL || '/api',
+  baseUrl: import.meta.env.VITE_API_URL || '/api',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token || localStorage.getItem('token');
     if (token) {
